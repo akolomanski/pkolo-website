0026b91be328295e19e2e751814557a471db8a36
Andrzej Kolomanski <jedrasek666@gmail.com>
2019-11-04 19:13:43 +0100

    initial work tamplate layout

--- src/templates/workTemplate.js
+++ src/templates/workTemplate.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+	margin-top: 50px;
+	margin-right: 100px;
+	margin-left: 100px;
+	img{
+		max-width: 800px;
+		float: right;
+	}
+`;
 
 const workTemplate = ({ pageContext: { data } }) => (
-	<div>
+	<StyledWrapper>
 		<Link to="/works">Go Back</Link>
 
 		<h1>{data.name}</h1>
@@ -10,6 +21,6 @@ const workTemplate = ({ pageContext: { data } }) => (
 		{data.photos.map((photo) => <img src={photo.url} alt="thumbnail" />)}
 
 		{console.log(data.name)}
-	</div>
+	</StyledWrapper>
 );
 export default workTemplate;
